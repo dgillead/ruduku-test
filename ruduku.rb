a889df29ad09748e51eb7bb6624ad653b088cc8f
Darnell Gillead <dlgillead@gmail.com>
2017-06-10 13:11:19 -0500

    ruduku and solve_board class

---
+++ ruduku.rb
@@ -0,0 +1,21 @@
+require_relative 'solve_board'
+
+class Rudoku
+  def initialize(board_string)
+    @board = board_string.split('')
+    @solution = SolveBoard.new(@board)
+  end
+
+  def board
+    @board.each_with_index do |element, index|
+      @board[index] = element.to_i
+    end
+    @board.each_slice(9).to_a
+  end
+end
+
+# The file has newlines at the end of each line, so we call
+# String#chomp to remove them.
+
+game = Rudoku.new('003020600900305001001806400008102900700000008006708200002609500800203009005010300')
+game.board
---
+++ solve_board.rb
@@ -0,0 +1,33 @@
+class SolveBoard
+  attr_accessor :original_board
+  attr_reader :empty_spots
+
+  def initialize(board_array)
+    @original_board = board_array
+    @empty_spots = find_empty_spots
+  end
+
+  def find_empty_spots
+    is_empty = []
+
+    original_board.each_with_index do |element, index|
+      if element == "0"
+        is_empty.push(index)
+      end
+    end
+
+    p is_empty
+  end
+
+  def find_solution
+    original_board.each_with_index do |element, index|
+      #if col_check && row_check && box_check
+        #original_board[element] += 1
+      #else
+        #original_board[element] -= 1
+        #index -= 1
+      #end
+    end
+
+  end
+end
