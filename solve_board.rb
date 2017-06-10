1dec55f7939aabea292f3467d7af596f8c91c82e
Jin Cheng <jincheng88@outlook.com>
2017-06-10 17:05:24 -0500

    ruduku

--- solve_board.rb
+++ solve_board.rb
@@ -1,38 +1,65 @@
+require 'pry'
+
 class SolveBoard
-  attr_accessor :original_board
+  attr_accessor :original_board, :final_board
   attr_reader :empty_spots
 
   def initialize(board_array)
     @original_board = board_array
+    @final_board = split_board
     @empty_spots = find_empty_spots
+    @possible_solution = find_solution
+    # binding.pry
+
+  end
+
+  def split_board
+    original_board.each_slice(9).to_a
   end
 
   def find_empty_spots
     is_empty = []
 
     original_board.each_with_index do |element, index|
-      if element == "0"
+      if element == 0
         is_empty.push(index)
       end
     end
 
-    p is_empty
+    is_empty
+    # binding.pry
   end
 
-  def row_check(number,index)
+  def row_check(number, index)
     row = index / 9
-    original_board[row].include?(number)
+    final_board[row].include?(number)
+    # binding.pry
   end
 
   def find_solution
-    original_board.each_with_index do |element, index|
-      #if col_check && row_check && box_check
-        #original_board[element] += 1
-      #else
-        #original_board[element] -= 1
-        #index -= 1
-      #end
+binding.pry
+    empty_spots.each_with_index do |element,index|
+# binding.pry
+      current_number = original_board[element]
+      current_number += 1
+      while current_number <= 9
+# binding.pry
+        if !row_check(current_number, element)
+          original_board[element] = current_number
+          break
+# binding.pry
+        else
+          current_number += 1
+# binding.pry
+        end
+      end
+# binding.pry
+      if current_number >= 9
+        original_board[element] = 0
+        index -= 1
+binding.pry
+      end
     end
-
   end
+
 end
